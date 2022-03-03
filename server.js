c1b4bbce2943f89482187c77fa29fefed527d2f2
betfie1@gmail.com <betfie1@gmail.com>
2022-03-03 18:32:52 -0500

    config express y listen

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const app = express();
 const path = require('path');
+const app = require('./src/app.js');
+
+console.log(path.join(__dirname + '/src/app.js'));
 
 app.get('/', (req, res ) => {
-    res.sendFile(path.join(__dirname + '/views/index.html'));
+    res.sendFile(path.join(__dirname + '/src/views/index.html'));
 });
 
-const server  = app.listen(process.env.PORT || 8080, () =>{
+async function main(){
+    const server = await app.listen(3000);
     const host = server.address().address;
     const port = server.address().port;
-
     console.log(`'listen in http://'${host}:${port}`);
-});
+};
+
+main();
