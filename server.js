e833e6f3ec9bc8e42171d1282b3d6456b2cafd5e
betfie1@gmail.com <betfie1@gmail.com>
2022-03-03 18:39:51 -0500

    ajuste listen

--- server.js
+++ server.js
@@ -7,11 +7,8 @@ app.get('/', (req, res ) => {
     res.sendFile(path.join(__dirname + '/src/views/index.html'));
 });
 
-async function main(){
-    const server = await app.listen(3000);
+const server  = app.listen(process.env.PORT || 8080, () =>{
     const host = server.address().address;
     const port = server.address().port;
     console.log(`'listen in http://'${host}:${port}`);
-};
-
-main();
+});
